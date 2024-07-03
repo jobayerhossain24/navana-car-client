496ee6c0bee3bebe920520b4b5980312c4fcb130
shaonkislo2 <shaon6340@gmail.com>
2024-07-03 00:39:22 -0700

    initial jwt processing done

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,9 +1,11 @@
 import { useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProviders";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  // console.log(location.pathname);
 
   if (loading) {
     return <progress className="progress w-56"></progress>;
@@ -12,7 +14,7 @@ const PrivateRoute = ({ children }) => {
   if (user?.email) {
     return children;
   }
-  return <Navigate to="login" replace></Navigate>;
+  return <Navigate state={location.pathname} to="/login" replace></Navigate>;
 };
 
 export default PrivateRoute;
